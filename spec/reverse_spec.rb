a38332c8256c9e355bb04ecd7730596cdd2879c9
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-08-04 01:38:04 +0430

    Test syntax corrected

--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -3,6 +3,6 @@ require_relative '../solver'
 describe 'from Solver class, reverse method' do
   solver = Solver.new
   it 'should reverse abc to cba' do
-    expect (solver.reverse('abc')).to eq('cba')
+    expect(solver.reverse('abc')).to eq('cba')
   end
 end
