f5b74760543f5c4871865f21c193fbeb14ee0d86
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-08-04 00:53:33 +0430

    Tests added for factorial

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -1,1 +1,9 @@
-# TDD first
+require_relative '../solver'
+
+describe 'Factorial' do
+  solver = Solver.new
+  it 'should return 1 if input is 0 or 1' do
+    expect(solver.factorial(0)).to eq(1)
+    expect(solver.factorial(1)).to eq(1)
+  end
+end
