e882f9b5c4e499c21b3e5f2a376d3787df79deb1
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-08-04 01:38:17 +0430

    Implemented code based on test

--- solver.rb
+++ solver.rb
@@ -9,7 +9,13 @@ class Solver
     end
     input * factorial(input - 1)
   end
+
+  def reverse(word)
+    reversed_word = ''
+    (word.size - 1).downto(0) do |i|
+      reversed_word << word[i]
+    end
+    reversed_word
+  end
 end
 
-solver = Solver.new
-puts solver.factorial(5)
