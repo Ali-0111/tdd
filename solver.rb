c9a2e3290525551304e73e6436db5031e83d0856
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-08-04 01:19:35 +0430

    Implementation for the postive values greater1

--- solver.rb
+++ solver.rb
@@ -1,11 +1,15 @@
 class Solver
   def factorial(input)
-    if (input < 0)
+    if input < 0
       raise ArgumentError, 'Input can not be negative, choose 0 or greater'
     end
-    
-    if (input == 0 || input == 1)
-      1
+
+    if input == 0 || input == 1
+      return 1
     end
+    input * factorial(input - 1)
   end
 end
+
+solver = Solver.new
+puts solver.factorial(5)
--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -2,12 +2,17 @@ require_relative '../solver'
 
 describe 'Factorial' do
   solver = Solver.new
+
+  it 'should raise error, if input is negative' do
+    expect { solver.factorial(-2)}.to raise_error(ArgumentError, 'Input can not be negative, choose 0 or greater')
+  end
+
   it 'should return 1 if input is 0 or 1' do
     expect(solver.factorial(0)).to eq(1)
     expect(solver.factorial(1)).to eq(1)
   end
 
-  it 'should raise error, if input is negative' do
-    expect { solver.factorial(-2)}.to raise_error(ArgumentError, 'Input can not be negative, choose 0 or greater')
+  it 'should return 5! = 120' do
+    expect(solver.factorial(5)).to eq(120)
   end
 end
