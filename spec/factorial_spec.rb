4f7151f52d82855fad3d6fcc42e1014b04e9a97d
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-08-04 01:07:55 +0430

    Test if input is negative

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -6,4 +6,8 @@ describe 'Factorial' do
     expect(solver.factorial(0)).to eq(1)
     expect(solver.factorial(1)).to eq(1)
   end
+
+  it 'should raise error, if input is negative' do
+    expect { solver.factorial(-2)}.to raise_error(ArgumentError, 'Input can not be negative, choose 0 or greater')
+  end
 end
